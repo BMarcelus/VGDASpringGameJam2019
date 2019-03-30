bec0ef947cd47e3f1745e50c281554e06e30d030
BMarcelus <bmdizon@uci.edu>
2019-03-30 16:12:32 -0700

    new shapes and makin money

--- Assets/OutlineManager.cs
+++ Assets/OutlineManager.cs
@@ -6,6 +6,8 @@ public class OutlineManager : MonoBehaviour
 {
     public GameObject[] outlines;
     public GameObject selected;
+    public float timer;
+    public bool waiting;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +17,20 @@ public class OutlineManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer>0) {
+          timer -= Time.deltaTime;
+        } else if(waiting) {
+          selected = outlines[Random.Range(0,outlines.Length)];
+          selected.transform.position = Vector3.zero;
+          selected.transform.Rotate(new Vector3(0,0, Random.Range(0, 360f)));
+          waiting = false;
+        }
     }
 
     public void pickRandomOutline() {
-      selected.SetActive(false);
-      selected = outlines[Random.Range(0,outlines.Length)];
-      selected.SetActive(true);
-      selected.transform.Rotate(new Vector3(0,0, Random.Range(0, 360f)));
+      // selected.SetActive(false);
+      selected.transform.position = Vector3.down * 100;
+      waiting = true;
+      timer = 0.2f;
     }
 }
