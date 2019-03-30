4f37ef0df5102d629481b28e30440c505baaca48
CheBlanco <ocelotking999@gmail.com>
2019-03-30 00:22:26 -0700

    Audio Prefab

--- Assets/Scripts/CloseButton.cs
+++ Assets/Scripts/CloseButton.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloseButton : MonoBehaviour{
-    public void doExitGame() {
+public class CloseButton : MonoBehaviour
+{
+    AudioManager audio;
+    
+    public void doExitGame() 
+    {
     Application.Quit();
-}}
+    Debug.Log("quits game");
+    }
+}
