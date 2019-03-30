0e445949f54a05a8278f01614fed2b28b4cba138
BMarcelus <bmdizon@uci.edu>
2019-03-30 01:53:38 -0700

    repivot shape assets and redo hover

--- Assets/Scripts/DragableObject.cs
+++ Assets/Scripts/DragableObject.cs
@@ -11,12 +11,15 @@ public class DragableObject : MonoBehaviour
     private Collider2D collider2d;
     private Vector3 startPosition;
     private bool startDrag;
+    private float scale = 1.2f;
+    private bool placed = false;
     // Start is called before the first frame update
     void Start()
     {
         collider2d = GetComponent<Collider2D>();
         gameObject.layer = 9;
         startDrag = false;
+        transform.localScale /= (1+scale)/2;
         // transform.localScale *= 0.5f;
     }
 
@@ -35,6 +38,9 @@ public class DragableObject : MonoBehaviour
             OnMouseDown();
           }
           startDrag = false;
+          if(inGrid) {
+            ObjectManager.HoverEffect.transform.position = transform.position;
+          }
         }
     }
 
@@ -48,6 +54,7 @@ public class DragableObject : MonoBehaviour
       startPosition = transform.position;
       beingDragged = true;
       startDrag = true;
+      ObjectManager.HoverEffect.SetActive(false);
       // transform.localScale *= 2;
 
     }
@@ -57,15 +64,18 @@ public class DragableObject : MonoBehaviour
       beingDragged = false;
       if(!inGrid) {
         transform.position = startPosition;
+        ObjectManager.HoverEffect.SetActive(false);
         // transform.localScale *= 0.5f;
       } else {
-      if(DebugManager.ClickDrag)Debug.Log("FINAL PLACEMENT", gameObject);
+        if(DebugManager.ClickDrag)Debug.Log("FINAL PLACEMENT", gameObject);
         mySpawner.Spawn();
         // collider2d.enabled = false;
         gameObject.layer = 0;
         GameObject effect = Instantiate(mySpawner.typeManager.PlaceEffect, transform.position, Quaternion.identity);
         Destroy(effect,2);
         transform.position += Vector3.forward*2;
+        ObjectManager.HoverEffect.SetActive(false);
+        placed = true;
         this.enabled = false;
       }
     }
@@ -74,9 +84,7 @@ public class DragableObject : MonoBehaviour
       if(DebugManager.Collisions)Debug.Log("TRIGGER ENTER", gameObject);
       if(col.tag == "BuildingGrid") {
         inGrid = true;
-        if(beingDragged) {
-          ObjectManager.HoverEffect.SetActive(false);
-        }
+        transform.localScale*=scale;
       }
     }
 
@@ -84,11 +92,14 @@ public class DragableObject : MonoBehaviour
       if(DebugManager.Collisions)Debug.Log("TRIGGER EXIT", gameObject);
       if(col.tag == "BuildingGrid") {
         inGrid = false;
+        transform.localScale/=scale;
+        ObjectManager.HoverEffect.SetActive(false);
       }
     }
 
     void OnMouseOver() {
-      if(inGrid)return;
+      if(placed)return;
+      if(beingDragged&&!inGrid)return;
       ObjectManager.HoverEffect.SetActive(true);
       ObjectManager.HoverEffect.transform.position = transform.position;
     }
