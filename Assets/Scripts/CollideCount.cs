12f333eed685224377104bf899112acd4a676cff
BMarcelus <bmdizon@uci.edu>
2019-03-30 09:38:37 -0700

    Effects for good and bad

--- Assets/Scripts/CollideCount.cs
+++ Assets/Scripts/CollideCount.cs
@@ -7,6 +7,7 @@ public class CollideCount : MonoBehaviour
     public int collideShape = 0;
     public int collideCount;
     public Manager gameManager;
+    private GridEffectsManager effectsManager;
     public GameObject grid;
 
     private bool targetValue;
@@ -16,6 +17,7 @@ public class CollideCount : MonoBehaviour
     {
         grid = GameObject.Find("Grid");
         gameManager = grid.GetComponent<Manager>();
+        effectsManager = grid.GetComponent<GridEffectsManager>();
         targetValue = false;
         isCorrect = true;
         gameManager.AddCollide();
@@ -29,11 +31,14 @@ public class CollideCount : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      if(collision.tag == "TargetShape") {
+      bool isTarget = collision.tag == "TargetShape";
+      if(isTarget) {
         if(!targetValue) gameManager.targetPositives += 1;
         targetValue = true;
       } else {
         collideShape += 1;
+        if(targetValue)effectsManager.QueuePositiveEffect(transform);
+        else effectsManager.QueueNegativeEffect(transform);
       }
       bool prevIsCorrect = isCorrect;
       isCorrect = (collideShape>0) == targetValue;
---
+++ Assets/Scripts/FadeAndDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAndDestroy : MonoBehaviour
+{
+    public float time;
+    private float totalTime;
+    private SpriteRenderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalTime = time;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time<=0) {
+          Destroy(gameObject);
+          return;
+        }
+        float alpha = time/totalTime;
+        Color c = rend.color;
+        c.a = alpha;
+        rend.color = c;
+    }
+}
