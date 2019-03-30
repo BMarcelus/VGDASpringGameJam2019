562cde2c1a1864ddeeba5e5c0319d70c150a2cf0
KingdomCross (Alex Chheng) <alexchheng1998@gmail.com>
2019-03-29 22:10:13 -0700

    Grid and Collision

---
+++ Assets/Scripts/CollideCount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideCount : MonoBehaviour
+{
+    public bool collideShape;
+    public int collideCount;
+    public Manager gameManager;
+    public GameObject grid;
+
+    void Awake()
+    {
+        grid = GameObject.Find("Grid");
+        gameManager = grid.GetComponent<Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collideShape = true;
+        gameManager.AddCollide();
+    }
+}
